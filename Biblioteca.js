1f3feece834964f31fe2e9058990cfd71540720a
Laura Nunes <lauracostanunes2@gmail.com>
2025-08-12 21:35:06 -0300

    Add files via upload

--- Biblioteca.js
+++ Biblioteca.js
@@ -1,33 +1,33 @@
 let biblioteca = [];
+let i = 0;
 for (let i = 0; i < 5; i++) {
     let titulo = prompt(`Informe o título do livro ${i + 1}:`);
     let autor = prompt(`Informe o autor do livro ${i + 1}:`);
-    let editora = prompt(`Informe a editora do livro ${i + 1}:`);
-    let idioma = prompt(`Informe o idioma do livro ${i + 1}:`);
     let ano = Number(prompt(`Informe o ano de publicação do livro ${i + 1}:`));
     let paginas = Number(prompt(`Informe o número de páginas do livro ${i + 1}:`));
-    let soma = 0
-    soma = paginas[i] + soma
+
 
     let livro = {
         titulo: titulo,
         autor: autor,
-        editora: editora,
-        idioma: idioma,
         ano: ano,
         paginas: paginas
     };
     biblioteca.push(livro);
 }
-let media = soma/5
+
+let maior = biblioteca[0]
+let soma = 0
+
 for (let i = 0; i < biblioteca.length; i++) {
     if (biblioteca[i].paginas > 300) {
         console.log(`${biblioteca[i].titulo} tem mais de 300 páginas.`);
     }
-}
-for (let i = 0; i < biblioteca.length; i++) {
-    if (biblioteca[i].paginas > biblioteca[0].paginas) {
-        console.log(`${biblioteca[i].paginas} tem maior número de páginas.`);
+    if (biblioteca[i].paginas > maior.paginas) {
+       maior = biblioteca[i]
     }
+    soma = soma + biblioteca[i].paginas
 }
+let media = soma/5
+console.log(`${biblioteca[i].titulo} tem maior número de páginas.`);
 console.log(`A média das páginas da biblioteca é: ${media}`)
