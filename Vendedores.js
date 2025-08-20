7836d2a2f9e09588094e5db9cf6d7f4cd90bf3e0
Laura Nunes <lauracostanunes2@gmail.com>
2025-08-20 20:32:37 -0300

    Add files via upload

--- Vendedores.js
+++ Vendedores.js
@@ -1,12 +1,16 @@
 let matriz = [];
-const vendedores = 4
-const dias = 7
+let nomesVendedores = [];
+for (let i = 0; i < 4; i++) {
+    let nome = prompt(`Digite o nome do vendedor ${i + 1}:`);
+    nomesVendedores.push(nome);
+}
 for (let i = 0; i < 4; i++) {;
     matriz[i] = [];
     for (let j = 0; j < 7; j++) {
         matriz[i][j] = parseInt(Math.random()*100)
     }
 }
+console.log(`Matriz de vendas: `);
 console.log(matriz)
 
 let somaVendedor = 0;
@@ -15,7 +19,7 @@ for (let i = 0; i < 4; i++) {
     for (let j = 0; j < 7; j++) {
         somaVendedor = somaVendedor + matriz[i][j]
     }
-    console.log(`Vendedor ${i+1} vendeu R$${somaVendedor}`)
+    console.log(`${nomesVendedores[i]} vendeu R$${somaVendedor}`);
 }
 
 let somaDia = 0;
@@ -30,21 +34,21 @@ for (let j = 0; j < 7; j++) {
 let maiorValor = matriz[0][0];
 let posicaoMaior = {vendedor: 0, dia: 0};
 
-for (let i = 0; i < vendedores; i++) {
-    for (let j = 0; j < dias; j++) {
+for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 7; j++) {
         if (matriz[i][j] > maiorValor) {
             maiorValor = matriz[i][j];
             posicaoMaior = {vendedor: i, dia: j};
         }
     }
 }
-console.log(`Maior valor de venda: R$${maiorValor} (Vendedor ${posicaoMaior.vendedor + 1}, Dia ${posicaoMaior.dia + 1})`);
+console.log(`Maior valor de venda: R$${maiorValor} (${nomesVendedores[posicaoMaior.vendedor]}, Dia ${posicaoMaior.dia + 1})`);
 
 let somaTotal = 0;
-for (let i = 0; i < vendedores; i++) {
-    for (let j = 0; j < dias; j++) {
+for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 7; j++) {
         somaTotal += matriz[i][j];
     }
 }
-let media = somaTotal / (vendedores * dias);
+let media = somaTotal / (4 * 7);
 console.log(`Média geral das vendas da semana: R$ ${media}`);
