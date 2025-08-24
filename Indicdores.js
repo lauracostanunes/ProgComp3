95d0caf3324ccd70858e78e12af40d7f0afc4e1e
Laura Nunes <lauracostanunes2@gmail.com>
2025-08-24 19:00:13 -0300

    Add files via upload

---
+++ Indicdores.js
@@ -0,0 +1,61 @@
+let matriz = [];
+for (let i = 0; i < 5; i++) {
+    matriz[i] = [];
+    for (let j = 0; j < 5; j++) {
+        matriz[i][j] = parseInt(Math.random()*100);
+    }
+}
+console.log(`Matriz de poluição: `);
+console.log(matriz);
+let somaDiagPrincipal = 0;
+let diagPrincipal = [];
+for (let i = 0; i < 5; i++) {
+    diagPrincipal.push(matriz[i][i]);
+    somaDiagPrincipal += matriz[i][i];
+}
+let mediaPrincipal = somaDiagPrincipal / 5;
+console.log(`Diagonal principal: `, diagPrincipal);
+console.log(`Média da diagonal principal: `, mediaPrincipal);
+let somaDiagSec = 0;
+let diagSec = [];
+for (let i = 0; i < 5; i++) {
+    diagSec.push(matriz[i][4 - i]);
+    somaDiagSec += matriz[i][4 - i];
+}
+let mediaSec = somaDiagSec / 5;
+console.log(`Diagonal secundária: `, diagSec);
+console.log(`Média da diagonal secundária: `, mediaSec);
+let maiorValor = matriz[0][0];
+let posicaoMaior = { regiao: 0, dia: 0 };
+for (let i = 0; i < 5; i++) {
+    for (let j = 0; j < 5; j++) {
+        if (matriz[i][j] > maiorValor) {
+            maiorValor = matriz[i][j];
+            posicaoMaior = { regiao: i, dia: j };
+        }
+    }
+}
+console.log(`Maior índice registrado: ${maiorValor} (Região ${posicaoMaior.regiao + 1}, Dia ${posicaoMaior.dia + 1})`);
+let contCritico = 0;
+for (let i = 0; i < 5; i++) {
+    for (let j = 0; j < 5; j++) {
+        if (matriz[i][j] > 300) {
+            contCritico++;
+        }
+    }
+}
+console.log(`Quantidade de vezes que o índice foi superior a 300: ${contCritico}`);
+let menorMedia = 0;
+let regiaoMenorMedia = 0;
+for (let i = 0; i < 5; i++) {
+    let somaRegiao = 0;
+    for (let j = 0; j < 5; j++) {
+        somaRegiao += matriz[i][j];
+    }
+    let mediaRegiao = somaRegiao / 5;
+    if (mediaRegiao < menorMedia) {
+        menorMedia = mediaRegiao;
+        regiaoMenorMedia = i;
+    }
+}
+console.log(`Região com menor média de poluição: Região ${regiaoMenorMedia + 1} (Média = ${menorMedia})`);
