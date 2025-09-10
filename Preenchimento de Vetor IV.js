c993d180aba0fa94c9f5959e1b6afb04a24a4a26
Laura Nunes <lauracostanunes2@gmail.com>
2025-09-10 20:21:49 -0300

    Add files via upload

--- Preenchimento de Vetor IV.js
+++ Preenchimento de Vetor IV.js
@@ -1,2 +1,35 @@
 var input = require('fs').readFileSync('/dev/stdin', 'utf8');
-var lines = input.trim().split('\n');
+var lines = input.split('\n').map(Number);
+
+let par = [];
+let impar = [];
+
+for (let i = 0; i < 15; i++) {
+  let num = lines[i];
+
+  if (num % 2 === 0) {
+    par.push(num);
+    if (par.length === 5) {
+      for (let j = 0; j < 5; j++) {
+        console.log(`par[${j}] = ${par[j]}`);
+      }
+      par = [];
+    }
+  } else {
+    impar.push(num);
+    if (impar.length === 5) {
+      for (let j = 0; j < 5; j++) {
+        console.log(`impar[${j}] = ${impar[j]}`);
+      }
+      impar = [];
+    }
+  }
+}
+
+for (let j = 0; j < impar.length; j++) {
+  console.log(`impar[${j}] = ${impar[j]}`);
+}
+
+for (let j = 0; j < par.length; j++) {
+  console.log(`par[${j}] = ${par[j]}`);
+}
--- Tamanho da Placa.js
+++ Tamanho da Placa.js
@@ -1,3 +1,19 @@
 var input = require('fs').readFileSync('/dev/stdin', 'utf8');
-var lines = input.trim().split('\n');
+var lines = input.split(/\s+/);
 
+let i = 0
+while (i < lines.length) {
+    let X = parseInt(lines[i++]) // Altura da placa da empresa
+    let Y = parseInt(lines[i++]) // Largura da placa da empresa
+    let P = parseInt(lines[i++]) // Quantidade de placas
+    for (let j = 0; j < P; j++) {
+        let Xi = parseInt(lines[i++]) // Altura da placa do usuário
+        let Yi = parseInt(lines[i++]) // Largura da placa do usuário
+        if ((Xi <= X && Yi <= Y) || (Xi <= Y && Yi <= X)) {
+            console.log('Sim')
+        }
+        else {
+            console.log('Nao')
+        }
+    }
+}
