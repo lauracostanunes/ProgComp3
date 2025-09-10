0fa3db9956dd996e75c9ed5cc3245c82f4af70dd
Laura Nunes <lauracostanunes2@gmail.com>
2025-09-10 20:35:28 -0300

    Add files via upload

--- Preenchimento de Vetor IV.js
+++ Preenchimento de Vetor IV.js
@@ -1,5 +1,5 @@
 var input = require('fs').readFileSync('/dev/stdin', 'utf8');
-var lines = input.split('\n').map(Number);
+var lines = input.split('\n').map(Number); // Cada elemento fo srrsy e criar um novo array com os resultados de uma função aplicada a cada elemnto
 
 let par = [];
 let impar = [];
@@ -13,7 +13,7 @@ for (let i = 0; i < 15; i++) {
       for (let j = 0; j < 5; j++) {
         console.log(`par[${j}] = ${par[j]}`);
       }
-      par = [];
+      par = []; // Esvazia par
     }
   } else {
     impar.push(num);
@@ -21,7 +21,7 @@ for (let i = 0; i < 15; i++) {
       for (let j = 0; j < 5; j++) {
         console.log(`impar[${j}] = ${impar[j]}`);
       }
-      impar = [];
+      impar = []; // Esvazia ímpar
     }
   }
 }
